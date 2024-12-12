51e72b532d16be4f7c02bc270a42592eb9b60867
Francisco De La Cruz <helloquico@gmail.com>
2024-12-12 07:28:15 +0100

    FastAPI Automatic Docs

--- ch02/todos/model.py
+++ ch02/todos/model.py
@@ -4,3 +4,13 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+    class Config:
+        json_schema_extra = {
+            "examples": [
+                {
+                    "id": 1,
+                    "item": "Example schema!",
+                }
+            ]
+        }

a8ef33d9dde1d980a5c902344d6a6c9ef5e779b4
Francisco De La Cruz <helloquico@gmail.com>
2024-12-12 12:36:55 +0100

    add PUT route

--- ch02/todos/model.py
+++ ch02/todos/model.py
@@ -14,3 +14,14 @@ class Todo(BaseModel):
                 }
             ]
         }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book",
+            }
+        }

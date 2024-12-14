c3abd48111255e71026381bf69c62034eecb94d1
Francisco De La Cruz <helloquico@gmail.com>
2024-12-14 21:07:14 +0100

    implement the models

---
+++ planner/models/users.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, EmailStr
+
+from models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }

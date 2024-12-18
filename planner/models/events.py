024bcb21c9d3d15a8356ef06667c20b3d8d34d0b
Francisco De La Cruz <helloquico@gmail.com>
2024-12-18 22:18:54 +0100

    update models files to include MongoDB docs

--- planner/models/events.py
+++ planner/models/events.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Optional
 
+from beanie import Document
 from pydantic import BaseModel
 
 
-class Event(BaseModel):
-    id: int
+class Event(Document):
     title: str
     image: str
     description: str
@@ -17,6 +17,28 @@ class Event(BaseModel):
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
                 "description": "We will be discussing the contents of the FastAPI book in this event. Ensure to come with your own copy to win gifts!",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet",
+            }
+        }
+
+    class Settings:
+        name = "events"
+
+
+class EventUpdate(BaseModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "FastAPI BookLaunch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.Ensure to come with your own copy to win gifts!",
                 "tags": ["python", "fastapi", "book", "launch"],
                 "location": "Google Meet",
             }

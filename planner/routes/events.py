77e78bdd7d05bbc8b81a3b3ec8f2759878ca0721
Francisco De La Cruz <helloquico@gmail.com>
2024-12-19 13:10:03 +0100

    fix put route

--- planner/routes/events.py
+++ planner/routes/events.py
@@ -37,12 +37,14 @@ async def create_event(body: Event = Body(...)) -> dict:
 
 @event_router.put("/{id}", response_model=Event)
 async def update_event(id: PydanticObjectId, body: EventUpdate) -> Event:
-    updated_event = await event_database.update(id)
+    updated_event = await event_database.update(id, body)
     if not updated_event:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Event with supplied ID does not exist",
         )
+
+    return updated_event
 
 
 @event_router.delete("/{id}")

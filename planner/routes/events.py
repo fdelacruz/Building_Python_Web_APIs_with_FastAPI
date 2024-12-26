05d2d0c1a423a0068af69b7408a0a5b9714bddb2
Francisco De La Cruz <helloquico@gmail.com>
2024-12-26 16:44:07 +0100

    update the events routes

--- planner/routes/events.py
+++ planner/routes/events.py
@@ -2,8 +2,9 @@ from typing import List
 
 from beanie import PydanticObjectId
 from database.connection import Database
-from fastapi import APIRouter, Body, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from models.events import Event, EventUpdate
+from auth.authenticate import authenticate
 
 event_router = APIRouter(tags=["Events"])
 
@@ -28,7 +29,7 @@ async def retrieve_event(id: PydanticObjectId) -> Event:
 
 
 @event_router.post("/new")
-async def create_event(body: Event = Body(...)) -> dict:
+async def create_event(body: Event, user: str = Depends(authenticate)) -> Event:
     await event_database.save(body)
     return {
         "message": "Event created successfully",
@@ -36,7 +37,9 @@ async def create_event(body: Event = Body(...)) -> dict:
 
 
 @event_router.put("/{id}", response_model=Event)
-async def update_event(id: PydanticObjectId, body: EventUpdate) -> Event:
+async def update_event(
+    id: PydanticObjectId, body: EventUpdate, user: str = Depends(authenticate)
+) -> Event:
     updated_event = await event_database.update(id, body)
     if not updated_event:
         raise HTTPException(
@@ -48,7 +51,7 @@ async def update_event(id: PydanticObjectId, body: EventUpdate) -> Event:
 
 
 @event_router.delete("/{id}")
-async def delete_event(id: PydanticObjectId) -> dict:
+async def delete_event(id: PydanticObjectId, user: str = Depends(authenticate)) -> dict:
     event = await event_database.delete(id)
     if not event:
         raise HTTPException(

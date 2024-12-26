f3fd313776f0e87fdf6282e265556639ceed259b
Francisco De La Cruz <helloquico@gmail.com>
2024-12-26 12:10:21 +0100

    set ip to 8080

--- planner/main.py
+++ planner/main.py
@@ -18,4 +18,4 @@ async def init_db():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)

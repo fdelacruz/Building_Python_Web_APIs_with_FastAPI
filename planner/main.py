d1c039a982485fd9791530aeb3ca60104b276c2a
Francisco De La Cruz <helloquico@gmail.com>
2024-12-27 22:29:22 +0100

    configure CORS

--- planner/main.py
+++ planner/main.py
@@ -1,12 +1,23 @@
 import uvicorn
 from database.connection import Settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.events import event_router
 from routes.users import user_router
 
 app = FastAPI()
 
 settings = Settings()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
